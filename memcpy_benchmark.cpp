2561ed1e1a740c4e1e010b7a6336393ddc21ac03
Ho Cheung <hoc@genebygene.com>
2017-01-26 13:37:33 -0600

    zlib inflate benchmark

--- memcpy_benchmark.cpp
+++ memcpy_benchmark.cpp
@@ -1,10 +1,14 @@
 #include <chrono>
 #include <cstring>
 #include <iostream>
+#include <thread>
+#include <vector>
+
+#include "asmlib.h"
 
 namespace {
     const size_t GB = 1073741824ull;
-    const size_t arr_size = GB * 8ull;
+    const size_t arr_size = GB * 1ull;
 }
 
 void large_memcpy_copy(char * arr) {
@@ -39,15 +43,9 @@ void small_memcpy_copy(char * arr, uint64_t block_size) {
     std::cout << block_size << " Bandwidth: " << (arr_size/2) / (std::chrono::duration_cast<std::chrono::nanoseconds>(time_taken).count() / (double)1000000000) / (1048576) << " MB/s " << accum << std::endl;
 }
 
-void naive_copy(char * arr) {
+void A_large_memcpy_copy(char * arr) {
     auto start = std::chrono::steady_clock::now();
-    for (uint64_t k = 0; k+arr_size/2+3 < arr_size; k+=4) {
-        arr[k + arr_size/2] = arr[k];
-        arr[k + arr_size/2 + 1] = arr[k+1];
-        arr[k + arr_size/2 + 2] = arr[k+2];
-        arr[k + arr_size/2 + 3] = arr[k+3];
-    }
-
+    A_memcpy(&arr[arr_size/2], &arr[0], arr_size/2);
     auto stop = std::chrono::steady_clock::now();
     auto time_taken = stop - start;
 
@@ -57,25 +55,60 @@ void naive_copy(char * arr) {
     }
     std::cout << accum << std::endl;
 
-    std::cout << "Time taken (naive copy): " << std::chrono::duration_cast<std::chrono::milliseconds>(time_taken).count() << " milliseconds" << std::endl;
-    std::cout << "Bandwidth: " << (arr_size/2) / (std::chrono::duration_cast<std::chrono::nanoseconds>(time_taken).count() / (double)1000000000) / (1'048'576) << " MB/s" << std::endl;
+    std::cout << "Time taken (large memcpy): " << std::chrono::duration_cast<std::chrono::milliseconds>(time_taken).count() << " milliseconds" << std::endl;
+    std::cout << "Bandwidth: " << (arr_size/2) / (std::chrono::duration_cast<std::chrono::nanoseconds>(time_taken).count() / (double)1000000000) / (1048576) << " MB/s" << std::endl;
+}
 
+void A_small_memcpy_copy(char * arr, uint64_t block_size) {
+    auto start = std::chrono::steady_clock::now();
+    for (uint64_t k = 0; k < arr_size/2; k+=block_size) {
+        A_memcpy(&arr[k+arr_size/2], &arr[k], block_size);
+    }
+    auto stop = std::chrono::steady_clock::now();
+    auto time_taken = stop - start;
+
+    uint64_t accum = 0;
+    for (uint64_t k = 0; k < arr_size; ++k) {
+        accum += arr[k];
+    }
+
+    std::cout << block_size << " Bandwidth: " << (arr_size/2) / (std::chrono::duration_cast<std::chrono::nanoseconds>(time_taken).count() / (double)1000000000) / (1048576) << " MB/s " << accum << std::endl;
 }
 
 int main() {
     std::cout << "Array size: " << arr_size << std::endl;
+auto memcpy_threadproc = [](){
     char * arr1 = new char[arr_size];
 
     std::memset(arr1, 1, arr_size);
 
-    large_memcpy_copy(arr1);
-    std::cout << std::endl;
-    for (uint64_t k = 1; k <= GB; k *= 2) {
-        small_memcpy_copy(arr1, k);
-    }
-    std::cout << std::endl;
-    large_memcpy_copy(arr1);
-    //naive_copy(arr1);
+//    large_memcpy_copy(arr1);
+//    std::cout << std::endl;
+//    for (uint64_t k = 1; k <= GB; k *= 2) {
+//        small_memcpy_copy(arr1, k);
+//    }
+//    std::cout << std::endl;
+//    large_memcpy_copy(arr1);
+//    std::cout << std::endl;
+
+    while (true)
+        A_large_memcpy_copy(arr1);
+//    std::cout << std::endl;
+//    for (uint64_t k = 1; k <= GB; k *= 2) {
+//        A_small_memcpy_copy(arr1, k);
+//    }
+//    std::cout << std::endl;
+//    A_large_memcpy_copy(arr1);
 
     delete[] arr1;
+};
+
+    std::vector<std::thread> thread_vec;
+    for (int i = 0; i < 8; ++i) {
+        thread_vec.emplace_back(memcpy_threadproc);
+    }
+
+    for (int i = 0; i < 8; ++i) {
+        thread_vec[i].join();
+    }
 }
---
+++ zlib_inflate_benchmark.cpp
@@ -0,0 +1,132 @@
+#include <fstream>
+#include <iostream>
+#include <exception>
+#include <vector>
+#include <thread>
+
+#include "zlib.h"
+
+namespace {
+    const std::string BAM_FILE_PATH = "/Users/hocheung20/genome_data/aengine/aengine.150x.bam";
+    const unsigned int NUM_THREADS = 8;
+    const unsigned int MAX_BLOCK_SIZE = 65536;
+}
+
+class BamReader {
+public:
+    BamReader(const std::string & filename) {
+        std::ifstream read_stream(filename, std::ios::binary | std::ios::ate);
+
+        file_size_ = read_stream.tellg();
+        std::cout << "File size: " << file_size_ << std::endl;
+        read_stream.seekg(0, std::ios::beg);
+
+        buffer_.resize(file_size_);
+        if (!read_stream.read(buffer_.data(), file_size_)) {
+            throw std::runtime_error("Error reading file into buffer!");
+        }
+
+        block_begin_it_ = buffer_.cbegin();
+    }
+
+    std::pair<std::vector<char>::const_iterator, uint32_t> getNextBlock() {
+        std::lock_guard<std::mutex> lock(next_block_mutex_);
+        if (block_begin_it_ == buffer_.cend())
+            return { buffer_.cend(), 0 };
+
+        uint16_t length = 0;
+        std::memcpy(&length, &*(block_begin_it_+16), sizeof(length));
+        length+=1;
+        auto ret = std::pair<std::vector<char>::const_iterator, uint32_t>(block_begin_it_, length);
+        block_begin_it_ += length;
+        return ret;
+    }
+
+    uint64_t getFileSize() {
+        return file_size_;
+    }
+
+private:
+    std::vector<char> buffer_;
+
+    std::mutex next_block_mutex_;
+    std::vector<char>::const_iterator block_begin_it_;
+
+    uint64_t file_size_;
+};
+
+int main() {
+    BamReader reader(BAM_FILE_PATH);
+
+    auto deflateProc = [&reader]() {
+        z_stream zs;
+        zs.zalloc = nullptr;
+        zs.zfree = nullptr;
+        int status = inflateInit2(&zs, -15);
+        if (status != Z_OK) {
+            throw std::runtime_error("Zlib initialization failed!");
+        }
+
+        while(true) {
+            auto block = reader.getNextBlock();
+
+            if (block.second == 0)
+                return;
+
+            std::vector<char>::const_iterator block_begin_it = block.first;
+            const uint32_t length = block.second;
+
+            if (*block_begin_it != (char)31 || *(block_begin_it+1) != (char)139) {
+                throw std::runtime_error("BgzfBlock header magic bytes don't match! Corrupt file?");
+            }
+
+            uint32_t uncompressed_size = 0;
+            std::memcpy(&uncompressed_size, &*(block_begin_it+length-4), sizeof(uncompressed_size));
+
+
+            zs.next_in = (Bytef *)&*(block_begin_it+18);
+            zs.avail_in = length - 16;
+
+            std::vector<char> uncompressed_data(MAX_BLOCK_SIZE);
+
+            zs.next_out = (Bytef *)uncompressed_data.data();
+            zs.avail_out = uncompressed_data.size();
+
+            auto inflate_status = inflate(&zs, Z_FINISH);
+            if (inflate_status != Z_STREAM_END) {
+                inflateEnd(&zs);
+                throw std::runtime_error("Zlib failed to decompress entire block!");
+            }
+
+            if (zs.total_out != uncompressed_size) {
+                std::cerr << "total_out: " << zs.total_out << " uncompressed_size: " << uncompressed_size << std::endl;
+                throw std::runtime_error("Uncompressed block size does not match expected!");
+            }
+
+            auto reset_status = inflateReset(&zs);
+            if (reset_status != Z_OK) {
+                throw std::runtime_error("Failed to reset zlib!");
+            }
+        }
+
+    };
+
+    std::vector<std::thread> thread_vec;
+    thread_vec.reserve(NUM_THREADS);
+
+    auto start = std::chrono::steady_clock::now();
+    for (int k = 0; k < NUM_THREADS; ++k) {
+        thread_vec.emplace_back(deflateProc);
+    }
+
+    for (auto & thread : thread_vec) {
+        thread.join();
+    }
+    auto stop = std::chrono::steady_clock::now();
+
+    double file_size_compressed_MB = reader.getFileSize() / (1024 * 1024.0);
+    double time = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() / 1000000000.0;
+
+    std::cout << "Deflating took: " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl;
+    std::cout << "Deflated file at " << file_size_compressed_MB / time << " compressed MB/s" << std::endl;
+}
